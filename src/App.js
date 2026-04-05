477a549b46ffe1e5277c07accc5abcd4a7bb9065
João Ferreira <faceami157@gmail.com>
2026-04-05 15:00:29 -0300

    feat: adiciona contador interativo ao app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
         <h1>Atividade 01 - DevOps</h1>
+
         <p>
           Este projeto demonstra o fluxo de versionamento <strong>Git Flow</strong>.
         </p>
+
         <p>
           Ambiente atual: <code>Desenvolvimento</code>
         </p>
+
+        {/* CONTADOR */}
+        <h2>{count}</h2>
+        <div>
+          <button onClick={() => setCount(count + 1)}>+</button>
+          <button onClick={() => setCount(count - 1)} style={{ marginLeft: '10px' }}>
+            -
+          </button>
+        </div>
+
+        <br />
+
         <a
           className="App-link"
-          href="https://github.com/seu-usuario" // Troque pelo seu link
+          href="https://github.com/Romanozx"
           target="_blank"
           rel="noopener noreferrer"
-        >
+        a>
           Ver Repositório no GitHub
         </a>
       </header>
